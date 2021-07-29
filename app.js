5bcb5e490527b0b602280ed1df4043953c190acc
jinwook jung <jinwook@pop-os.localdomain>
2021-07-29 22:13:45 +0900

    [3.1] HTML in Javascript

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-document.title = "hello! from JS!";
+const title = document.getElementById("title"); // h1 tag의 id인 title에서 정보를 읽어옴.  HTML에서 id 를 할당해야 찾을 수 있음.
+
+// console.dir(title);
+title.innnerText = "Got you!"; // HTML의 값을 갱신하게됨.(HTML이 바뀜) Grab me -> Got you
