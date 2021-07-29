025eaf456fdbc22eef6439b138d8d2ada2b3daa5
jinwook jung <jinwook@pop-os.localdomain>
2021-07-30 00:14:25 +0900

    [3.4] Events 2

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ function handleTitleClick(){
     title.style.color = "blue";
 }
 
+function handleMouseEnter(){
+    title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone";
+}
+
 // console.dir(title); 
 
-title.addEventListener("click", handleTitleClick);
+title.addEventListener("click", handleTitleClick); // element를 가져와서 event listener 실행. 두번째 인자는 callbock function
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave); 
 
