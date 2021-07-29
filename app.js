d37957e074561eda7a7d9b14981971da49e80f41
jinwook jung <jinwook@pop-os.localdomain>
2021-07-30 00:43:07 +0900

    [3.5] More Events(window)

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick(){
     console.log("title was clicked");
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter(){
-    title.innerText = "Mouse is here";
+    h1.innerText = "Mouse is here";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone";
+    h1.innerText = "Mouse is gone";
 }
 
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato"; // document.body는 html의 body부분
+}
+
+function handleWindowCopy() {
+    alert("copier!!");
+}
+
+function handleWindowOffline() {
+    alert("SOS");
+}
+
+function handleWindowOffline() {
+    alert("ALL GOOOD");
+}
 // console.dir(title); 
 
-title.addEventListener("click", handleTitleClick); // element를 가져와서 event listener 실행. 두번째 인자는 callbock function
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave); 
+h1.addEventListener("click", handleTitleClick); // element를 가져와서 event listener 실행. 두번째 인자는 callbock function
+//title.onclick = handleTitleClick; // 위와 동일. add Event listener를 쓰면 나중에 remove도 가능
+h1.addEventListener("mouseenter", handleMouseEnter);
+//title.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave); 
 
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline); // offline될때 활성화
+window.addEventListener("online", handleWindowOffline); // 다시 online 될때 활성화
