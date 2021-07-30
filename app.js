8508526f70614ba4925e2adac3583d14385d8585
jinwook jung <jinwook@pop-os.localdomain>
2021-07-30 18:15:39 +0900

    [3.7] CSS in JS(better coding style)

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue"){
-        newColor = "tomato";
+    const clickedClass = "clicked sexy-font"
+    if(h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "blue";
-    } 
-    h1.style.color = newColor;
+        h1.className = clickedClass;
+    }
 }
 
 h1.addEventListener("click", handleTitleClick); // element를 가져와서 event listener 실행. 두번째 인자는 callbock function
