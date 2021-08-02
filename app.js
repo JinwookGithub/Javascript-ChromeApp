36770c37e2418255e96c3376d65b3f977553c890
jinwook jung <jinwook@pop-os.localdomain>
2021-08-03 01:29:39 +0900

    [3.8] CSS in JS(using API)

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick(){
-    const clickedClass = "clicked"
-    if(h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked"); // 한번만 나오니까 변수없이 이름 바로 씀.
 }
 
 h1.addEventListener("click", handleTitleClick); // element를 가져와서 event listener 실행. 두번째 인자는 callbock function
