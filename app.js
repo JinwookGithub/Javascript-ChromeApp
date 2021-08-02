71718ab53519cdc47c36f0997d587f0358b8d6d6
jinwook jung <jinwook@pop-os.localdomain>
2021-08-03 01:33:25 +0900

    RESET

--- app.js
+++
@@ -1,7 +0,0 @@
-const h1 = document.querySelector(".hello:first-child h1");
-
-function handleTitleClick(){
-    h1.classList.toggle("clicked"); // 한번만 나오니까 변수없이 이름 바로 씀.
-}
-
-h1.addEventListener("click", handleTitleClick); // element를 가져와서 event listener 실행. 두번째 인자는 callbock function
