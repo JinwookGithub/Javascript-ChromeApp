6f6ef35f67ff0e9ceba037aae5337949bda39e7c
jinwook jung <jinwook@pop-os.localdomain>
2021-08-03 21:37:16 +0900

    [4.2] Events

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // const loginForm = document.querySelector("#login-form");
 // const loginInput = loginform.querySelector("input"); // loginForm의 타입이 HTML Element 이므로 loginForm에서도 querySelector 사용가능
 // const loginButton = loginform.querySelector("button");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    //console.dir(loginInput.value); // document의 value에 입력 값 있음
-    const username = loginInput.value;
-    console.log(username);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(loginInput.value);
 }
-loginButton.addEventListener("click", onLoginBtnClick)
+
+loginForm.addEventListener("submit", onLoginSubmit);
