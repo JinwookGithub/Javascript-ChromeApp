abbef78058f8fa33f469674935ec937075dd8d36
jinwook jung <jinwook@pop-os.localdomain>
2021-08-03 23:35:38 +0900

    [4.4] Getting Username

--- app.js
+++ app.js
@@ -3,18 +3,17 @@
 // const loginButton = loginform.querySelector("button");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username;// string 연산. 아래와 동일.
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);    
-}
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
