ff2ce470643239810b9708e4009047515e32a38e
jinwook jung <jinwook@pop-os.localdomain>
2021-08-04 00:10:52 +0900

    [4.5] Saving Username

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     // greeting.innerText = "Hello " + username;// string 연산. 아래와 동일.
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
