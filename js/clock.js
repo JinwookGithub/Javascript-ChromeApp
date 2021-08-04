d92efbebb84a98702d575d0c27f75a8ca276fcf6
jinwook jung <jinwook@pop-os.localdomain>
2021-08-04 16:52:31 +0900

    [5.1] Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,10 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date(); // 매번 객체를 생성
+    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
 }
 
-setInterval(sayHello, 5000); // 매 5초마다 실행
+// setTimeout(sayHello, 5000); // 5초뒤에 1번 실행
+getClock(); // 1번은 실행. 없으면 1초뒤에 나타남
+setInterval(getClock, 1000); // 그다음 1초마다 실행
