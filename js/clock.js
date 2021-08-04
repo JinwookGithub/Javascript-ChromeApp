027a2f73b6579b2d6de09335b9802af261c25662
jinwook jung <jinwook@pop-os.localdomain>
2021-08-04 17:56:20 +0900

    [5.2] Padstart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date(); // 매번 객체를 생성
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = (`${hours}:${minutes}:${seconds}`);
 }
 
 // setTimeout(sayHello, 5000); // 5초뒤에 1번 실행
