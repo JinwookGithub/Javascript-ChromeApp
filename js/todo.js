f56cb2020c8524579c05b86cf0488cb00f9d946c
jinwook jung <jinwook@pop-os.localdomain>
2021-08-06 02:06:12 +0900

    [7.2] Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event){
+    // console.dir(event.target); // target : HTML element clicked
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-
-    li.appendChild(span);
     span.innerText = newTodo;
-    console.log(li);
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);    
     toDoList.appendChild(li);
 }
 
