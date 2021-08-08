3dd007f4d22ab546dc0781e352e25d66d84bbeb7
jinwook jung <jinwook@pop-os.localdomain>
2021-08-08 23:12:46 +0900

    [8.1] Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+
+const API_KEY = "d97d906def1f4cd53e8f2f388554cc16";
+
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const log = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+    fetch(url)// url을 호출. promise이므로 응답을 기다림 then()이후로 처리
+    .then(response => response.json())
+    .then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main}/${data.main.temp}`;
+    }); 
 }
 
 function onGeoError(){
